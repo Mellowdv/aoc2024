0c69b3c2addf88a33656822a4d0d237775512327
Mellowdv <69904426+Mellowdv@users.noreply.github.com>
2024-12-16 22:10:29 +0100

    add solution to p2, sadness

--- day16.cpp
+++ day16.cpp
@@ -1,6 +1,7 @@
-// Work in progress
+// Work in progress, clean this dumb shit up...
 #include <bits/stdc++.h>
-constexpr std::size_t SZ = 15;
+#include <cstdint>
+constexpr std::size_t SZ = 141;
 static std::array<std::array<char, SZ>, SZ> grid {};
 
 struct pair_hash {
@@ -50,7 +51,6 @@ constexpr std::array<std::array<int64_t, 4>, 4> direction_to_cost {{
 bool in_bounds(std::pair<int64_t, int64_t> p) {
     return (0 <= p.first && p.first < SZ && 0 <= p.second && p.second < SZ);
 }
-
 
 int64_t dijkstra(std::pair<int64_t, int64_t> start) {
     std::array<std::array<int64_t, SZ>, SZ> cost_map {};
@@ -110,31 +110,22 @@ int64_t dijkstra(std::pair<int64_t, int64_t> start) {
     return cost_map[1][SZ - 2];
 }
 
-void collect_uniques(
-        std::unordered_set<std::pair<int64_t, int64_t>, pair_hash> &uniques,
-        std::array<std::array<std::vector<std::tuple<int64_t, int64_t, Direction>>, SZ>, SZ> const &prevs, std::pair<int64_t, int64_t> p,
-        std::array<std::array<std::array<int64_t, 4>, SZ>, SZ> &cost_map) {
-    if (prevs[p.first][p.second].empty()) {
-        return;
-    }
-    for (auto &tup: prevs[p.first][p.second]) {
-        std::pair<int64_t, int64_t> new_p = {std::get<0>(tup), std::get<1>(tup)};
-        Direction dir = std::get<2>(tup);
-        std::array<int64_t, 4> costs {{
-            direction_to_cost[dir][E],
-            direction_to_cost[dir][W],
-            direction_to_cost[dir][N],
-            direction_to_cost[dir][S]
-        }};
-        for (auto &cost: costs) {
-        }
-        if (uniques.contains(new_p)) continue;
-        uniques.insert(new_p);
-        collect_uniques(uniques, prevs, new_p, cost_map);
+std::pair<int64_t, int64_t> direction_to_reverse_delta(Direction d) {
+    switch (d) {
+        case E:
+            return {0, -1};
+        case W:
+            return {0, 1};
+        case N:
+            return {1, 0};
+        case S:
+            return {-1, 0};
+        default:
+            std::cout << "Bad dir!\n";
+            return {0, 0};
     }
 }
-
-int64_t dijkstra_3d(std::pair<int64_t, int64_t> start) {
+int64_t dijkstra_3d(std::pair<int64_t, int64_t> start, int64_t p1_result) {
     std::array<std::array<std::array<int64_t, 4>, SZ>, SZ> cost_map {};
     for (auto &row: cost_map) {
         for (auto &col: row) {
@@ -156,7 +147,6 @@ int64_t dijkstra_3d(std::pair<int64_t, int64_t> start) {
         auto const current_move = q.top();
         auto const current_coords = current_move.coords;
         q.pop();
-        visited_map[current_coords.first][current_coords.second].set(current_move.dir);
         std::array<Move, 4> adjacent_points {{
             {{current_coords.first + 1, current_coords.second},
               direction_to_cost[current_move.dir][N], N},
@@ -169,32 +159,61 @@ int64_t dijkstra_3d(std::pair<int64_t, int64_t> start) {
         }};
         for (auto &p: adjacent_points) {
             const auto &coords {p.coords};
-            if (visited_map[coords.first][coords.second].test(p.dir)) continue;
             if (grid[coords.first][coords.second] == '#') {
                 continue;
             }
             auto const cost = direction_to_cost[current_move.dir][p.dir];
+            if (cost == 2001) continue; // This means we're doubling back, pointless
             if (cost_map[current_coords.first][current_coords.second][current_move.dir] + cost < cost_map[coords.first][coords.second][p.dir]) {
+                std::cout << "Setting cost for " << coords.first << ", " << coords.second << ", " << p.dir << "; " <<
+                    cost_map[current_coords.first][current_coords.second][current_move.dir] + cost << "\n";
+                std::cout << "Coming from " << current_coords.first << ", " << current_coords.second << ", " << current_move.dir << "\n";
                 cost_map[coords.first][coords.second][p.dir] = cost_map[current_coords.first][current_coords.second][current_move.dir] + cost;
                 p.cost = cost_map[current_coords.first][current_coords.second][current_move.dir] + cost;
                 q.push(p);
-                prevs[coords.first][coords.second].push_back(std::make_tuple(current_coords.first, current_coords.second, current_move.dir));
+                prevs[coords.first][coords.second].push_back(std::make_tuple(current_coords.first, current_coords.second, p.dir));
             } else {
                 continue;
             }
         }
     }
-    for (auto cost: cost_map[1][SZ - 2]) {
-        std::cout << cost << "\n";
-    }
-    for (auto prev: prevs[7][5]) {
-        std::cout << std::get<0>(prev) << ", " << std::get<1>(prev) << ", " << std::get<2>(prev) << "\n";
-        std::cout << cost_map[std::get<0>(prev)][std::get<1>(prev)][std::get<2>(prev)] << "\n";
-    }
+
     std::unordered_set<std::pair<int64_t, int64_t>, pair_hash> uniques {};
-    collect_uniques(uniques, prevs, {1, SZ - 2}, cost_map);
+    std::stack<std::tuple<int64_t, int64_t, Direction>> s {};
+    uniques.insert({1, SZ - 2});
+    for (auto const &[x, y, d]: prevs[1][SZ - 2]) {
+        std::cout << cost_map[x][y][d] << " ";
+        if (cost_map[x][y][d] == p1_result - 1 ||
+            cost_map[x][y][d] == p1_result - 1001) {
+            s.push({x, y, d});
+        }
+    }
+    std::cout << "\n";
+    while (!s.empty()) {
+        auto const cur = s.top();
+        s.pop();
+
+        const auto &[x, y, d] = cur;
+        uniques.insert({x, y});
+        auto current_cost = cost_map[x][y][d];
+        std::cout << "Current cost: " << current_cost << "\n";
+        std::cout << "Prevs for " << x << ", " << y << ":\n";
+        for (auto const &[nx, ny, nd]: prevs[std::get<0>(cur)][std::get<1>(cur)]) {
+            std::cout << nx << ", " << ny << ", " << nd << ", cost: " << cost_map[nx][ny][nd] << "\n";
+            int dir_counter = 0;
+            for (auto const item: cost_map[nx][ny]) {
+                std::cout << "Found cost " << item << "\n";
+                if (cost_map[x][y][d] - 1001 == item ||
+                    cost_map[x][y][d] - 1 == item) {
+                    s.push({nx, ny, Direction(dir_counter)});
+                }
+                dir_counter++;
+            }
+        }
+        std::cout << "\n";
+    }
     std::cout << uniques.size() << "\n";
-    return cost_map[1][SZ - 2][0];
+    return 0;
 }
 
 int main() {
@@ -217,5 +236,6 @@ int main() {
     }
     std::cout << start.first << ", " << start.second << "\n";
     int64_t p1_result = dijkstra(start);
-    int64_t p2_result = dijkstra_3d(start);
+    std::cout << p1_result << "\n";
+    int64_t p2_result = dijkstra_3d(start, p1_result);
 }
